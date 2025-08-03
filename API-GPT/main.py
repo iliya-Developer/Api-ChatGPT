e563df0cdbf7ae5a09888140a7668b6079906c27
i-dev <ishadowdev@gmail.com>
2025-08-03 12:27:16 -0700

    Add files via upload

--- API-GPT/main.py
+++ API-GPT/main.py
@@ -138,4 +138,4 @@ def index():
     return jsonify({"result" : 'Flask is working!'}), 200
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True)
